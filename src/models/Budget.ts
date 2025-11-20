f53f8098ece1eab5d1475763dcbae1e782dead69
Joseph Perez <jp535673@gmail.com>
2025-11-20 17:48:00 -0500

    first commit

---
+++ src/models/Budget.ts
@@ -0,0 +1,19 @@
+import { Table, Column, Model, DataType, HasMany, ForeignKey,  } from "sequelize-typescript";
+
+@Table({
+    tableName: 'budgets'
+})
+
+class Budget extends Model {
+    @Column({
+        type: DataType.STRING(100)
+    })
+    name: string
+
+    @Column({
+        type: DataType.DECIMAL
+    })
+    amount: number
+}
+
+export default Budget
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express' 
+import colors from 'colors'
+import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync() // Nos crea las tablas y las columnas en automatico
+        console.log(colors.blue.bold('Conexión exitosa a la DB'));
+        
+    } catch (error) {
+        //console.log(error);
+        console.log(colors.red.bold('Falló la conexion a la DB'));       
+    }
+}
+
+connectDB()
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+
+
+export default app
