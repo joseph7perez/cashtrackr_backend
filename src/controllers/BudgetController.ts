d21b518c908f4239c381ebf662fc984e7e59030a
Joseph Perez <jp535673@gmail.com>
2025-11-20 21:48:32 -0500

    Crecion metodos de Rest API para budgetController y actualizar las rutas del router

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -10,4 +10,15 @@ export class BudgetController {
         console.log('Desde /api/budgets create');
     }
 
+    static getById = async (req: Request, res: Response) => {
+        console.log('Desde /api/budgets getById');
+    }
+
+    static updateById = async (req: Request, res: Response) => {
+        console.log('Desde /api/budgets updateById');
+    }
+
+    static deleteById = async (req: Request, res: Response) => {
+        console.log('Desde /api/budgets deleteById');
+    }
 }
