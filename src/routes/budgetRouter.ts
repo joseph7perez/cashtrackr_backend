1d8c7cfcaa3be0df4135a433b7bd5b761960ba15
Joseph Perez <jp535673@gmail.com>
2025-11-21 21:02:23 -0500

    Obtencion de los datos en create y guardarlos en la BD y creando su respectiva validacion

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+router.post('/', 
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('amount')
+        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacio')
+        .isNumeric().withMessage('El valor debe ser un número')
+        .custom(value => value > 0).withMessage('El valor debe ser mayor a 0'),
+    handleInputErrors,
+    BudgetController.create)
+
 router.get('/:id', BudgetController.getById)
 router.put('/:id', BudgetController.updateById)
 router.delete('/:id', BudgetController.deleteById)
