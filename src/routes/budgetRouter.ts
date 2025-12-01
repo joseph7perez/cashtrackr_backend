31cc82320d0cc4117c1788c442d44d60c304f7dc
Joseph Perez <jp535673@gmail.com>
2025-12-01 18:04:35 -0500

    Crear validaciones en el budgetRouter para updateById

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -21,7 +21,19 @@ router.get('/:id',
         .custom(value => value > 0).withMessage('ID no válido'),
     handleInputErrors,
     BudgetController.getById)
-router.put('/:id', BudgetController.updateById)
+
+router.put('/:id', 
+    param('id'). isInt().withMessage('ID no válido')
+        .custom(value => value > 0).withMessage('ID no válido'),
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('amount')
+        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacio')
+        .isNumeric().withMessage('El valor debe ser un número')
+        .custom(value => value > 0).withMessage('El valor debe ser mayor a 0'),
+    handleInputErrors,
+    BudgetController.updateById)
+
 router.delete('/:id', BudgetController.deleteById)
 
 export default router
