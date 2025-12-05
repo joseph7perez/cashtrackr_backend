245fe31562279d78c010909c70e99b97d718c716
Joseph Perez <jp535673@gmail.com>
2025-12-05 16:00:46 -0500

    Crear trycatch para guardar los gastos, en el metodo create

--- src/controllers/ExpenseController.ts
+++ src/controllers/ExpenseController.ts
@@ -1,12 +1,20 @@
 import type { Request, Response } from 'express'
+import Expense from '../models/Expense'
 
 export class ExpenseController {
-    static getAll = async (req: Request, res: Response) => {
-    
-    }
   
     static create = async (req: Request, res: Response) => {
-     
+        //console.log(req.budget.id); // Obtener el id del budget al que pertenece el expense
+        try {
+            const expense = new Expense(req.body)
+            expense.budgetId = req.budget.id 
+            await expense.save()
+            res.status(201).json('Gasto Creado Correctamente')
+
+        } catch (error) {
+            //console.log(error);
+            res.status(500).json({error: 'Hubo un Error '})      
+        }
     }
   
     static getById = async (req: Request, res: Response) => {
